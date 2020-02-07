54119869f4a5a031e6b348e844a7e42827749e24
Aleksandr Galchenkov <galchenkov@gmail.com>
2020-02-07 18:49:49 +0300

    Отрефакторил получение скрина чата

--- Screen.ts
+++ Screen.ts
@@ -3,12 +3,43 @@ import { ButtonProps } from './blocks/Button'
 import { FooterBlock, FooterProps } from './blocks/Footer'
 import { SearchBlock, SearchProps } from './blocks/Search'
 import { ChatiumChildNode, flattenChildren } from './utils/children'
+import { Icon } from './blocks/commonTypes'
+
+interface ChatProps {
+  messages_get_url: string
+  messages_add_url: string
+  messages_edit_url: string
+  messages_delete_url: string
+  mark_as_read_url: string
+  last_read_get_url: string
+  support_paging: boolean
+  files_put_url: string
+  reply_quotes_enabled: boolean
+  current_author: ChatUser
+  group_author: ChatUser | null
+  messages_socket_id: string
+  last_read_at: number | null
+  last_read_socket_id: string
+  typing_socket_data: {
+    id: string
+    name: string
+    uid: string
+  }
+}
+
+interface ChatUser {
+  id: string
+  url: string
+  name: string
+  avatar: Icon
+}
 
 export interface ChatiumScreen {
   title: string
   description?: string
   headerButton?: Pick<ButtonProps, 'icon' | 'onClick'>
-  blocks: ChatiumBlock[]
+  blocks?: ChatiumBlock[]
+  chat?: ChatProps
   footer?: FooterProps
   search?: SearchProps
 }
