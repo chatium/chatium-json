521dcbbcefd0d0bad58a66fb92444ec2d2fc4a2d
Timur Karimbaev <timur@karimbaev.com>
2020-03-11 18:34:30 +0300

    Сделал поддержку дочерних Entity для страниц (переходы на ссылки, создание заказа и т.п.)

--- actions/navigate.ts
+++ actions/navigate.ts
@@ -2,6 +2,7 @@ export interface NavigateAction {
   type: 'navigate'
   url: string
   replace?: boolean
+  openInExternalApp?: boolean
 }
 
 export function navigate(url: string, replace = false): NavigateAction {
