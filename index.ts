302dc60ab11c5caa5096088a536d371011188285
davojan <davojan@gmail.com>
2020-03-13 15:22:48 +0300

    backend: small refactor

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export { ChatiumActions, apiCall, goBack, navigate, refresh, resetSearch, showToast } from './actions'
-export { Audio, Box, Button, ChatiumBlock, Image, ListItem, Search, Text, Video, Form, Field } from './blocks'
+export { Audio, Box, Button, ChatiumBlock, Image, ListItem, Search, Slot, Text, Video, Form, Field } from './blocks'
 export { ChatiumJsonKey, Icon, WithKey } from './blocks/commonTypes'
+export { injectSlots } from './blocks/Slot'
 export { ChatiumScreen, Screen } from './Screen'
 export { jsx } from './utils/jsx'
