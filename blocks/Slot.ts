d5ced6aa5f4495e6bb92806e109c1865ef5aa038
davojan <davojan@gmail.com>
2020-03-13 17:25:08 +0300

    backend: chatium-json: improved injectSlots api to properly support Promises and simplified slot types

--- blocks/Slot.ts
+++ blocks/Slot.ts
@@ -1,36 +1,83 @@
-import { CommonBlockProps } from 'lib/chatium-json/blocks/commonTypes'
+import { types } from 'util'
 
-import { ChatiumScreen, ScreenProps } from '../Screen'
+import { ChatiumScreen } from '../Screen'
+import { WithKey } from './commonTypes'
 
 type SlotTarget = 'left' | 'right'
-export type SlotsProps = Record<SlotTarget, Omit<SlotBlock, 'type' | 'target'>>
+export type SlotsProps = Record<string, SlotInfo>
 
-export interface SlotProps extends CommonBlockProps {
+export type SlotInfo = { url: string; screen?: never } | { screen: ChatiumScreen; url?: never }
+export type AsyncSlotInfo = { url: string; screen?: never } | { screen: ChatiumScreen; url?: never }
+
+export type SlotProps = SlotInfo & {
   target: SlotTarget
-  url?: string
-  screen?: ScreenProps
 }
 
-export interface SlotBlock extends SlotProps {
-  type: 'slot'
-}
+export type SlotBlock = SlotProps &
+  WithKey & {
+    type: 'slot'
+  }
 
-export async function Slot(props: SlotProps): Promise<SlotBlock> {
-  const screen = await props.screen
-
+export async function Slot(props: AsyncSlotInfo & { target: SlotTarget }): Promise<SlotBlock> {
   return {
     type: 'slot',
-    ...props,
-    screen,
+    target: props.target,
+    ...(props.screen ? { screen: await props.screen } : { url: props.url }),
   }
 }
 
+/**
+ * Injects the given slots into the slots of the given screen. If the screen already has same slot, it is overriden
+ * Slots can be given as:
+ *  - string - interpreted as slot.url
+ *  - ChatiumScreen - interpreted as slot.screen
+ *  - SlotInfo - as-is
+ *  - Promise of anything above - auto-resolved
+ * @param screen
+ * @param slots
+ */
 export async function injectSlots(
   screen: Promise<ChatiumScreen>,
-  slots: Record<string, Omit<SlotBlock, 'type' | 'target'>>,
+  slots: Record<string, SlotContent | Promise<SlotContent>>,
 ): Promise<ChatiumScreen> {
+  const resolvedScreen = await screen
+  const resolvedSlots = await normalizeSlots(slots)
   return {
-    ...(await screen),
-    slots,
+    ...resolvedScreen,
+    slots: {
+      ...resolvedScreen.slots,
+      ...resolvedSlots,
+    },
   }
+}
+
+type SlotContent = SlotInfo | ChatiumScreen | string
+
+async function normalizeSlots(slots: Record<string, SlotContent | Promise<SlotContent>>): Promise<SlotsProps> {
+  const result = {} as SlotsProps
+
+  for (const slot in slots) {
+    const content = await slots[slot]
+    result[slot] = isPromise(content) ? normalizeSlot(await content) : normalizeSlot(content)
+  }
+
+  return result
+}
+
+function normalizeSlot(slotContent: SlotContent): SlotInfo {
+  if (typeof slotContent === 'string') {
+    return { url: slotContent }
+  } else if (isSlotInfo(slotContent)) {
+    return slotContent
+  } else {
+    return { screen: slotContent }
+  }
+}
+
+function isPromise(b: SlotContent | Promise<SlotContent>): b is Promise<SlotContent> {
+  return types.isPromise(b)
+}
+
+function isSlotInfo(val: any): val is SlotInfo {
+  return val && (typeof val.screen === 'object' || typeof val.url === 'string')
 }
