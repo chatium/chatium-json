7913e7c0963699dd437b886db91879199a698cfd
Aleksandr Galchenkov <galchenkov@gmail.com>
2020-03-26 23:13:05 +0300

    Рефакторинг ссылки пользователя url на onCLick: ChatiumActions

--- utils/transformUserToJson.ts
+++ utils/transformUserToJson.ts
@@ -1,11 +1,12 @@
+import { AccountCtx } from 'core/account'
 import { getUserDisplayName, User } from 'core/user'
 import { Author } from 'lib/chatium-json/blocks/commonTypes'
 
-export function transformUserToJson(user: User): Author {
+export function transformUserToJson(ctx: AccountCtx, user: User): Author {
   return {
     id: user.id,
-    url: user.id,
     name: getUserDisplayName(user),
     avatar: user.avatar,
+    onClick: [ctx.account.navigate(`user/${user.username ? user.username : user.id}`)],
   }
 }
