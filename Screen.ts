6f2f1e3c61a85c239207df084c227f374e122c0d
Aleksandr Galchenkov <galchenkov@gmail.com>
2020-05-20 12:38:27 +0300

    [backend + desktop] Переделка ура получения контекстного меню

--- Screen.ts
+++ Screen.ts
@@ -39,7 +39,7 @@ export interface ChatProps {
     update_url?: string
   }
   render_inverted?: boolean
-  get_context_action_url: string
+  on_context_api_call_url: string
 }
 
 export interface ChatiumScreen {
