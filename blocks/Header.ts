db8bdb2ec848d547a2eded74b5ad42f0cd95c327
Timur Karimbaev <timur@karimbaev.com>
2020-07-04 15:56:35 +0300

    change

--- blocks/Header.ts
+++ blocks/Header.ts
@@ -22,17 +22,17 @@ export interface HeaderBlock extends HeaderProps {
   type: 'header'
 }
 
-type Props = Omit<HeaderProps, 'blocks' | 'title' | 'description'> & {
+type Props = Omit<HeaderProps, 'title' | 'description'> & {
   title?: TextBlock | Promise<TextBlock>
   description?: TextBlock | Promise<TextBlock>
 }
 
 export async function Header(props: Props, ...children: ChatiumChildNode[]): Promise<HeaderBlock> {
-  const blocks = await flattenChildren(children)
+  const blocks = children.length ? await flattenChildren(children) : props.blocks
   return {
     type: 'header',
     ...props,
-    blocks: blocks.length ? blocks : undefined,
+    blocks: blocks && blocks.length ? blocks : undefined,
     title: await props.title,
     description: await props.description,
   }
