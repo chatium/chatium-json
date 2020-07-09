c74a8fd8f6e495b78dd94c5a14b768abad48fe3b
Timur Karimbaev <timur@karimbaev.com>
2020-07-09 14:16:21 +0300

    change

--- blocks/commonTypes.ts
+++ blocks/commonTypes.ts
@@ -14,6 +14,17 @@ export const emptyContainerStyle = {
   paddingRight: 0,
 }
 
+export const noMarginContainerStyle = {
+  paddingTop: 0,
+  paddingBottom: 0,
+  paddingLeft: 0,
+  paddingRight: 0,
+  marginTop: 0,
+  marginBottom: 0,
+  marginLeft: 0,
+  marginRight: 0,
+}
+
 export type InputType =
   | 'String'
   | 'Integer'
