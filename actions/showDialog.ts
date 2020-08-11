72846c3952d3818a08eca5171c86b4aa2e51b8c7
Timur Karimbaev <timur@karimbaev.com>
2020-08-11 19:32:18 +0300

    change

--- actions/showDialog.ts
+++ actions/showDialog.ts
@@ -1,3 +1,8 @@
+import { LangCtx } from 'core/i18n'
+
+import { Color, ColorPallete } from '../blocks/commonTypes'
+import { apiCall, ChatiumAction, ContextMenuItem, showContextMenu } from './index'
+
 interface ShowDialogCommon {
   type: 'showDialog'
   title?: string
@@ -30,4 +35,27 @@ export function showCodeDialog(options: Omit<ShowDialogInputTypeText, 'type' | '
     inputType: 'text',
     ...options,
   }
+}
+
+export function showColorSelector(ctx: LangCtx, props: { submitUrl: string; value: Color }): ChatiumAction {
+  const colors = [
+    { label: ctx.t('Синий'), value: ColorPallete.Primary },
+    { label: ctx.t('Черный'), value: ColorPallete.Black },
+    { label: ctx.t('Белый'), value: ColorPallete.White },
+    { label: ctx.t('Зеленый'), value: ColorPallete.Success },
+    { label: ctx.t('Красный'), value: ColorPallete.Danger },
+    { label: ctx.t('Оранжевый'), value: ColorPallete.RedOrange },
+    { label: ctx.t('Темно-желтый'), value: ColorPallete.Waiting },
+  ]
+  const menuItems: ContextMenuItem[] = []
+
+  for (const color of colors) {
+    menuItems.push({
+      title: color.label + (props.value === color.value ? ` [${ctx.t('Выбрано')}]` : ''),
+      onClick: apiCall(props.submitUrl, {
+        value: color.value,
+      }),
+    })
+  }
+  return showContextMenu(menuItems)
 }
