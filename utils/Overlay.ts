c6d7859ba393cffb57998e8265c33d74b7624c70
Timur Karimbaev <timur@karimbaev.com>
2020-08-20 04:10:03 +0300

    chnage

--- utils/Overlay.ts
+++ utils/Overlay.ts
@@ -2,6 +2,7 @@ import { ChatiumBlock } from '../blocks'
 import { ContainerStyle } from '../blocks/commonTypes'
 
 export interface OverlayProps {
+  id?: string
   gradientColors?: string[]
   blocks: ChatiumBlock[]
   containerStyle?: ContainerStyle
