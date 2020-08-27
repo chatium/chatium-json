4920308e50881282d9798ed6fa2f9c03b97cc90e
davojan <davojan@gmail.com>
2020-08-27 12:02:19 +0300

    backend: chatium-json: move experimental gallery-related stuff

--- blocks/Gallery.ts
+++ blocks/Gallery.ts
@@ -1,5 +1,5 @@
 import type { ChatiumActions } from '../actions'
-import type { ChatiumJsonId, CommonBlockProps, ContainerStyle, OverlayProps } from '../commonTypes'
+import type { CommonBlockProps } from '../commonTypes'
 import { ChatiumChildNode, flattenChildren } from '../utils/children'
 import type { ChatiumBlock } from './index'
 
@@ -22,27 +22,6 @@ export async function Gallery(props: GalleryProps, ...children: ChatiumChildNode
     })
   }
 
-  return {
-    type: 'gallery',
-    ...props,
-    slides,
-  }
-}
-
-export interface GallerySlide {
-  id?: ChatiumJsonId // for updateCurrentScreenBlock action support
-  entityId?: string
-  blocks?: ChatiumBlock[]
-  content?: ChatiumBlock
-  overlay?: OverlayProps
-  containerStyle?: ContainerStyle
-  stretchContent?: boolean
-  onBecomeVisible?: ChatiumActions
-  onVideoEnd?: ChatiumActions
-  onContext?: ChatiumActions
-}
-
-export async function SlidesGallery(props: GalleryProps, slides: GallerySlide[]): Promise<GalleryBlock> {
   return {
     type: 'gallery',
     ...props,
